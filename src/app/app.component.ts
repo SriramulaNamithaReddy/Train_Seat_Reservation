b51dc2a4e3dba5a94aff472edd59206d8d444893
SriramulaNamithaReddy <namithareddy82@gmail.com>
2024-10-16 19:59:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SeatReservationComponent } from './seat-reservation/seat-reservation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SeatReservationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
